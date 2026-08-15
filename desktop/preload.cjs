1eea2fcd925b00d4780f8391210a3a00c65e8649
ghandhitechnology <ghandhitechnology@users.noreply.github.com>
2026-08-15 16:14:36 +0900

    Hide the macOS title bar in the Electron shell and inset the studio menu bar around the traffic lights.

--- desktop/preload.cjs
+++ desktop/preload.cjs
@@ -2,4 +2,11 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('rhwpDesktop', {
   ensureAgentHub: () => ipcRenderer.invoke('agent-hub:ensure'),
+  platform: process.platform,
+  isFullScreen: () => ipcRenderer.invoke('window:is-fullscreen'),
+  onFullScreenChange: (callback) => {
+    ipcRenderer.on('window:fullscreen-changed', (_event, fullscreen) => {
+      callback(Boolean(fullscreen));
+    });
+  },
 });
