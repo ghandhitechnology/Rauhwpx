c438f44ffd855fbac053c4b0c8835d7580367dee
Andy <heemang12bo@gmail.com>
2026-09-25 01:50:02 +0900

    chore(renderer): match main on #344 conflict files so the branch can merge

--- rhwp/src/renderer/layout/shape_layout.rs
+++ rhwp/src/renderer/layout/shape_layout.rs
@@ -1,1 +1,9 @@
-SEE_NEXT
+//! 도형/글상자/그룹 개체 레이아웃
+
+use super::super::composer::{compose_paragraph, reflow_line_segs, ComposedParagraph};
+use super::super::float_placement::{float_exclusion, horizontal_range, FloatPlacementContext};
+use super::super::page_layout::LayoutRect;
+use super::super::pagination::PageItem;
+use super::super::render_tree::*;
+use super::super::style_resolver::ResolvedStyleSet;
+use super::super::{hwpunit_to_px, px_to_hwpunit, PathCommand, ShapeStyle, TextStyle};
